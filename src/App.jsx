6dd19334d01a454abcba47458443a3c97df8db0b
luiggylml <luiggylml@gmail.com>
2022-03-26 13:55:13 -0500

    rework

--- src/App.jsx
+++ src/App.jsx
@@ -1,60 +1,50 @@
-import { useState,useEffect } from 'react'
-import logo from './logo.svg'
-import './App.css'
+import { useState, useEffect } from "react";
+import "./App.css";
+import dice from "./assets/icon-dice.svg";
+import pattern from "./assets/pattern-divider-desktop.svg";
 
 function App() {
-  const [quote, setQuote] = useState('') 
-  const [author, setAuthor] = useState('')
-  var colors = [
-    '#16a085',
-    '#27ae60',
-    '#2c3e50',
-    '#f39c12',
-    '#e74c3c',
-    '#9b59b6',
-    '#FB6964',
-    '#342224',
-    '#472E32',
-    '#BDBB99',
-    '#77B1A9',
-    '#73A857'
-  ];
+  const [advice, setAdvice] = useState("");
+  const [id, setId] = useState("");
 
   useEffect(() => {
-    fetch('https://api.quotable.io/random')
-    .then(res => res.json())
-    .then(data => {
-      setQuote(data.content)
-      setAuthor(data.author)
-    })
-  }, [])
+    fetch("https://api.adviceslip.com/advice")
+      .then((res) => res.json())
+      .then((data) => {
+        setAdvice(data.slip.advice);
+        setId(data.slip.id);
+      });
+  }, []);
 
-  const getQuote = () => {
-    fetch('https://api.quotable.io/random')
-      .then(res => res.json())
-      .then(data => {
-        setQuote(data.content)
-        setAuthor(data.author)
-      })
-  }
-
-  
-  var randomColor = colors[Math.floor(Math.random() * colors.length)];
-
+  const getAdvice = () => {
+    fetch("https://api.adviceslip.com/advice")
+      .then((res) => res.json())
+      .then((data) => {
+        setAdvice(data.slip.advice);
+        setId(data.slip.id);
+      });
+  };
 
   return (
-    <>  
-    <div className="App" style={{ backgroundColor: randomColor }}>
-      <div className="box-quote">
-        <h1 style={{ color: randomColor }}>{quote}</h1>
-        <p style={{ color: randomColor }}>-{author}</p>
-        <input style={{ backgroundColor: randomColor }} type="submit" value='New Quote' onClick={getQuote}/>
+    <>
+      <div className="App">
+        <div className="box-quote">
+          <p> ADVICE #{id}</p>
+          <h1>{advice}</h1>
+          <img className="pattern" src={pattern} alt="" />
+
+          <button
+            type="button"
+            value="New Quote"
+            className="button"
+            onClick={getAdvice}
+          >
+            <img className="dice" src={dice} alt="" />
+          </button>
+        </div>
       </div>
-      
-    </div>
-    
     </>
-  )
+  );
 }
 
-export default App
+export default App;
